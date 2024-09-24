e569a8a57847b25379254f8ef92785d6423ff118
Mustafa Idris <81088966+mustafa0taru@users.noreply.github.com>
2024-09-24 16:18:41 +0100

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,13 +1,3 @@
--- models/staging/stg_customers.sql
-
--- Set the materialization strategy for the model
-{{ config(
-  materialized='table',
-  database='raw',
-  schema='ecommerce'
-) }}
-
--- Your transformation logic here, for example:
 WITH customers AS (
   SELECT
     customerID,
